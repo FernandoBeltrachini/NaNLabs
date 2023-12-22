dfd7c9afa95132bd62fdb7049ae54d2e7815e6fe
Fernando Beltrachini <viejo@macviejo.fibertel.com.ar>
2023-12-22 08:12:18 -0300

    Adding integration test

--- src/test/java/com/fernet/spacex/controller/CardControllerTest.java
+++ src/test/java/com/fernet/spacex/controller/CardControllerTest.java
@@ -3,24 +3,49 @@ package com.fernet.spacex.controller;
 import com.fernet.spacex.service.CardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class CardControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private CardService cardService;
     @Test
-    public void testCreateCardEndpoint() throws Exception {
+    public void testCreateIssue() throws Exception {
         String requestBody = "{\"type\": \"issue\", \"description\": \"description\", \"tittle\": \"tittle\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testCreateBug() throws Exception {
+        String requestBody = "{\"type\": \"bug\", \"description\": \"description\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testCreateTask() throws Exception {
+        String requestBody = "{\"type\": \"task\", \"category\": \"Test\", \"tittle\": \"tittle\"}";
 
         mockMvc.perform(MockMvcRequestBuilders.post("/")
                         .contentType(MediaType.APPLICATION_JSON)
