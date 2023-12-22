75a0697bf8d0e71451f9150b461c3e5c2f15bcaf
Fernando Beltrachini <viejo@macviejo.fibertel.com.ar>
2023-12-22 07:39:32 -0300

    Removing warning

--- src/main/java/com/fernet/spacex/service/rest/model/TrelloLabelListItem.java
+++ src/main/java/com/fernet/spacex/service/rest/model/TrelloLabelListItem.java
@@ -1,8 +1,10 @@
 package com.fernet.spacex.service.rest.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class TrelloLabelListItem extends TrelloListItem{
     private TrelloLabelsColors color;
 }
